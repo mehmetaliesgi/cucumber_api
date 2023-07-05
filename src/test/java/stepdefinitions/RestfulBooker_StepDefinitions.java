26ba8a3752c273e8a4c45eb53896d77634d39096
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-07-05 13:31:40 +0300

    RestfulBooker endpointine spesifik olarak bir rezervasyon testi yazıldı

--- src/test/java/stepdefinitions/RestfulBooker_StepDefinitions.java
+++ src/test/java/stepdefinitions/RestfulBooker_StepDefinitions.java
@@ -2,6 +2,7 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import utilities.ConfigReader;
@@ -11,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 
 public class RestfulBooker_StepDefinitions {
     Response response;
-
+    JsonPath jsonPathResponse;
     @Given("GET requesti yapılır ve response degerini kaydeder")
     public void doRequestAndSaveResponse() {
         response = given().when().get(jphStepdefinitions.endpoint);
@@ -23,6 +24,18 @@ public class RestfulBooker_StepDefinitions {
     }
     @Then("Request sonucu response'da content type degerinin {string}")
     public void checkContentType(String contentType) {
+
+    }
+    @Then("Response attribute değerlerinin {string}, {string}, {int}, {string}, {string}, {string} olduğu kontrol edilir")
+    public void checkResponseAttributes(String firstname, String lastname, Integer totalprice, String checkin, String checkout, String additionalneeds) {
+        jsonPathResponse = response.jsonPath();
+
+        assertEquals(firstname, jsonPathResponse.getString("firstname"));
+        assertEquals(lastname, jsonPathResponse.getString("lastname"));
+        assertEquals(totalprice,(Integer) jsonPathResponse.get("totalprice"));
+        assertEquals(checkin, jsonPathResponse.getString("bookingdates.checkin"));
+        assertEquals(checkout, jsonPathResponse.getString("bookingdates.checkout"));
+        assertEquals(additionalneeds, jsonPathResponse.getString("additionalneeds"));
 
     }
 }
--- src/test/resources/features/RestfulBooker_API.feature
+++ src/test/resources/features/RestfulBooker_API.feature
@@ -9,4 +9,10 @@ Feature: Kullanıcı Restful-booker endpoint'inin testini gerçekleştirir.
     Then Request sonucu response'da status degerinin 200 olduğu kontrol edilir
 
 
+  Scenario: Kullanıcı GET isteği yaparak spesifik bir rezervasyonu görüntüler
+    And Path parametreleri icin "booking/7" kullanir
+    When GET requesti yapılır ve response degerini kaydeder
+    Then Request sonucu response'da status degerinin 200 olduğu kontrol edilir
+    And Response attribute değerlerinin "Mary", "Wilson", 254, "2015-12-25", "2023-06-05", "Breakfast" olduğu kontrol edilir
+
 
